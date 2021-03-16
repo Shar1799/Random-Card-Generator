1850ae5d4dff562c5a0f63926e5946fd5b616315
Shar1799 <sharmey99@gmail.com>
2021-03-16 23:45:32 +0000

    Carta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let guardar; //variable para almacenar símbolo generado
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".parteSuperior").innerHTML = simboloAleatorio();
+  document.querySelector(".parteInferior").innerHTML = almacenarSimbolo();
+  document.querySelector(".numero").innerHTML = numeroAleatorio();
+};
+
+var simboloAleatorio = () => {
+  var simbolo = ["♦", "♥", "♠", "♣"]; //Arreglo con símbolos
+  var generadorSimbolo = [Math.floor(Math.random() * simbolo.length)]; //generador de aleatorio
+  guardar = simbolo[generadorSimbolo];
+
+  //Retorna
+  return simbolo[generadorSimbolo];
+};
+
+var almacenarSimbolo = () => {
+  return guardar;
+};
+
+var numeroAleatorio = () => {
+  var numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ]; //Arreglo con números
+  var generadorNumero = [Math.floor(Math.random() * numero.length)]; //generador de aleatorio
+
+  //Retorna
+  return numero[generadorNumero];
 };
